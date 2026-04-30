d809b9d8a150fda03b8c479c8217746b71a222d2
Lucas Epherra <letoso13@gmail.com>
2026-04-30 10:53:00 -0300

    import resuelto

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
